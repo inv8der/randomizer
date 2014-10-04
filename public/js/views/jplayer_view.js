08053ffc78ca1b1208dfb98bfe0f9d9bd2c7cad6
Marc Ryan <ryanmq87@gmail.com>
2014-10-04 15:17:53 -0700

    Created a jplayer Ember View to manage audio playback.

---
+++ public/js/views/jplayer_view.js
@@ -0,0 +1,42 @@
+App.JPlayerView = Ember.View.extend({
+    tagName: 'div',
+    classNames: ['jplayer'],
+
+    didInsertElement: function() {
+        // Once the view is rendered, initialize jplayer so we can play de music
+        this.$().jPlayer({
+            swfPath: 'js/libs',
+            supplied: 'rtmpa'
+        });
+
+        var controller = this.get('controller');
+        controller.on('mediaChanged', this, this.playTrack);
+    },
+
+    willDestroyElement: function() {
+        var controller = this.get('controller');
+        controller.off('mediaChanged', this, this.playTrack);
+    },
+
+    playTrack: function(track_id) {
+        var _this = this;
+
+        this.stopPlayback();
+        $.ajax({
+            url: '/audio/' + track_id,
+            dataType: 'json',
+            success: function(data) {
+                _this.$().jPlayer('setMedia', {
+                    rtmpa: data.location + '/' + data.resource
+                });
+                _this.$().jPlayer('play');
+            }
+        });
+    },
+
+    stopPlayback: function() {
+        this.$().jPlayer('clearMedia');
+    }
+});
+
+Ember.Handlebars.helper('jplayer', App.JPlayerView);
