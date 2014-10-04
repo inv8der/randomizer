31adcccce9640d59c940926f6255344e592ee99b
Marc Ryan <ryanmq87@gmail.com>
2014-10-04 15:36:39 -0700

    Hooked in the real backend for my client app using the Ember RESTAdapter. This required some changes to my Ember models and app controller. Also added a library for managing cookies in the document.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,21 @@
 window.App = Ember.Application.create();
 
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
+Ember.Application.initializer({
+    name: 'loginHandler',
+    initialize: function(container, application) {
+        $(window).on('message', function(event) {
+            var data = event.originalEvent.data;
+            if (data._id) {
+                var controller = container.lookup('controller:randomizer');
+                controller.send('loginUser', data);
+            }
+        });
+    }
+});
+
+App.ApplicationSerializer = DS.RESTSerializer.extend({
+    primaryKey: '_id'
+});
 
 App.Router.map(function() {
     this.route('randomizer', {path: '/'});
@@ -9,22 +24,27 @@ App.Router.map(function() {
 App.RandomizerRoute = Ember.Route.extend({
     model: function() {
         return Ember.RSVP.hash({
-            user: this.store.find('user', 1),
+            user: this.store.find('user', docCookies.getItem('user') || 0),
             genres: this.store.find('genre')
         });
+    },
+    actions: {
+        refreshView: function() {
+            this.refresh();
+        }
     }
 });
 
 // TODO: Make sure genre has at least one featured track. Otherwise,
 // don't consider it during randomization. Also, don't show it on the
 // settings page
-App.RandomizerController = Ember.ObjectController.extend({
+App.RandomizerController = Ember.ObjectController.extend(Ember.Evented, {
     actions: {
         updateGenreWeight: function(id, value) {
             var user = this.get('user');
-            user.get('genreWeights').then(function(genreWeights) {
+            user.get('weights').then(function(genreWeights) {
                 var genreWeight = genreWeights.findBy('id', id);
-                genreWeight.set('weight', value);
+                genreWeight.set('value', value);
             });
         },
 
@@ -33,12 +53,11 @@ App.RandomizerController = Ember.ObjectController.extend({
                 genres = this.get('genres'),
                 _this = this;
 
-            user.get('genreWeights').then(function(genreWeights) {
+            user.get('weights').then(function(genreWeights) {
                 var randomizeArr, winner_id, winner;
                     
                 randomizeArr = genreWeights.reduce(function(previousValue, item) {
-                    var i;
-                    for (i=0; i<item.get('weight'); i++) {
+                    for (var i=0; i<item.get('value'); i++) {
                         previousValue.push(item.get('genre').get('id'));
                     }
                     return previousValue;
@@ -47,17 +66,18 @@ App.RandomizerController = Ember.ObjectController.extend({
                 winner_id = randomizeArr[Math.floor(Math.random()*randomizeArr.length)];
                 winner = genres.findBy('id', winner_id);
 
-                winner.get('featuredTracks').then(function(featuredTracks) {
+                winner.get('tracks').then(function(featuredTracks) {
                     var i = Math.floor(Math.random()*featuredTracks.get('length')),
                         chosenOne = featuredTracks.toArray()[i];
 
                     _this.set('nowPlaying', chosenOne);
+                    console.log('Winner: ' + winner_id);
+                    _this.trigger('mediaChanged', chosenOne.get('id'));
 
                     // TODO: Find a way to move this out of the controller
                     $('#randomizer').css('background-image', 'url(' + chosenOne.get('image') + ')');
 
-                    user.get('playlists').then(function(playlists) {
-                        var playlist = playlists.findBy('name', 'Randomizer');
+                    user.get('playlist').then(function(playlist) {
                         playlist.get('tracks').then(function(tracks) {
                             if (tracks.contains(chosenOne)) {
                                 _this.set('isFavorited', true);
@@ -71,6 +91,13 @@ App.RandomizerController = Ember.ObjectController.extend({
             });
         },
 
+        loginUser: function(user) {
+            docCookies.setItem('user', user._id);
+
+            this.set('displayLoginPrompt', false);
+            this.get('target').send('refreshView');
+        },
+
         like: function() {
             var nowPlaying = this.get('nowPlaying'),
                 user = this.get('user'),
@@ -78,8 +105,7 @@ App.RandomizerController = Ember.ObjectController.extend({
 
             // TODO: Change user model so that it only contains one playlist, the
             // Randomizer playlist
-            user.get('playlists').then(function(playlists) {
-                var playlist = playlists.findBy('name', 'Randomizer');
+            user.get('playlist').then(function(playlist) {
                 playlist.get('tracks').then(function(tracks) {
                     if (tracks.contains(nowPlaying)) {
                         tracks.removeObject(nowPlaying);
@@ -98,6 +124,25 @@ App.RandomizerController = Ember.ObjectController.extend({
             //console.log(nowPlaying);
         },
 
+        manageSession: function() {
+            var _this = this,
+                isUserAuthenticated = this.get('isUserAuthenticated');
+
+            if (isUserAuthenticated === false) {
+                var displayLoginPrompt = this.get('displayLoginPrompt');
+                this.set('displayLoginPrompt', !displayLoginPrompt); 
+            } else {
+                // Logout user
+                $.ajax({
+                    url: '/logout',
+                    success: function() {
+                        docCookies.removeItem('user');
+                        _this.get('target').send('refreshView');
+                    }
+                })
+            }
+        },
+
         togglePlaylist: function() {
             var isVisible = this.get('isPlaylistVisible');
             this.set('isPlaylistVisible', !isVisible);
@@ -109,12 +154,15 @@ App.RandomizerController = Ember.ObjectController.extend({
         }
     },
 
-    playTrack: function(track_id) {
+    isUserAuthenticated: function() {
+        var user = this.get('user'),
+            user_id = user.get('id');
 
-    },
+        return (user_id === '0') ? false : true;
+    }.property('user.id'),
 
     nowPlaying: undefined,
-
+    displayLoginPrompt: false,
     isPlaylistVisible: false,
     isSettingsVisible: false
 });
